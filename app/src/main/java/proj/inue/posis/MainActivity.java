1e510902a90c002bfd76f74eb112111d006442c5
jp0800 <angelthis.john@gmail.com>
2024-05-05 23:46:19 +0800

    :+1: Added Filtering for Name, Price and quantity UNTRACKED FILES

---
+++ app/src/main/java/proj/inue/posis/MainActivity.java
@@ -0,0 +1,52 @@
+package proj.inue.posis;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Arrays;
+
+import proj.inue.posis.utils.SQLiteDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static SQLiteDatabase sqLiteDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        try {
+            sqLiteDatabase = new SQLiteDatabase(this);
+            sqLiteDatabase.onUpgrade(sqLiteDatabase.getWritableDatabase(), 1, 2);
+            
+            Toast.makeText(this, "Setup Complete", Toast.LENGTH_SHORT).show();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000); // 3000 is the delayed time in milliseconds.
+    }
+}
